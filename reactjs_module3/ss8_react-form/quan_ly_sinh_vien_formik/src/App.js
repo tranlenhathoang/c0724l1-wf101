9f515be71615c5ae7f4c68e216b38fa2bcc3370b
Apple <tranlenhathoang357@gmail.com>
2024-12-17 09:46:57 +0700

    thêm toast

--- reactjs_module3/ss8_react-form/quan_ly_sinh_vien_formik/src/App.js
+++ reactjs_module3/ss8_react-form/quan_ly_sinh_vien_formik/src/App.js
@@ -5,10 +5,12 @@ import Login from './Login/Login';
 import HeaderStudent from './Main/Header';
 import ListStudent from './Main/ListStudent';
 import AddStudent from './Main/AddStudent';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
+      <ToastContainer/>
     <Routes>
       <Route path={'/'} element={<Login/>}></Route>
       <Route path={'/homepage'} element={<HeaderStudent/>}/>
